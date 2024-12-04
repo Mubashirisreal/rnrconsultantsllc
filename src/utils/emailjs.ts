413dfa9625bad1cc7600a43df7f79f4f1a685c19
Mubashirisreal <147941230+Mubashirisreal@users.noreply.github.com>
2024-12-04 18:36:47 +0530

    Update emailjs.ts

--- src/utils/emailjs.ts
+++ src/utils/emailjs.ts
@@ -3,8 +3,8 @@ import emailjs from '@emailjs/browser';
 export const sendEmail = async (formData: any) => {
   try {
     const response = await emailjs.send(
-      'service_clkyqaw', // Replace with your EmailJS service ID
-      'template_vqaulfn', // Replace with your EmailJS template ID
+      'service_p6afizd', // Replace with your EmailJS service ID
+      'template_oca9vqd', // Replace with your EmailJS template ID
       formData,
       'slO0_D96dBm1DXJhK' // Replace with your EmailJS public key
     );
